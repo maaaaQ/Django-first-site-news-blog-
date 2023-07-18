b3d0818832e1c630a30706a9c43c0dc471671826
Yuri Makeev <makeevyuri7@gmail.com>
2023-07-18 12:49:17 +0300

    Full

---
+++ mysite/news/forms.py
@@ -0,0 +1,23 @@
+from .models import Articles
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
+
+
+class ArticlesForm(ModelForm):
+    class Meta:
+        model = Articles
+        fields = ["title", "anons", "full_text", "date"]
+
+        widgets = {
+            "title": TextInput(
+                attrs={"class": "form-control", "placeholder": "Название статьи"}
+            ),
+            "anons": TextInput(
+                attrs={"class": "form-control", "placeholder": "Анонс статьи"}
+            ),
+            "date": DateTimeInput(
+                attrs={"class": "form-control", "placeholder": "Дата публикациии"}
+            ),
+            "full_text": Textarea(
+                attrs={"class": "form-control", "placeholder": "Полный текст статьи"}
+            ),
+        }
